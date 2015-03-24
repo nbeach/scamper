519ec327e21745ca58a2759626d2a1d63f58f013
nbeach <nicholascbeach@gmail.com>
2015-03-23 20:05:56 -0400

    Adjusted mapping for recipe uploads to be consistent with other controllers

--- src/main/java/com/nicholasbeach/scamper/controller/ReceiptController.java
+++ src/main/java/com/nicholasbeach/scamper/controller/ReceiptController.java
@@ -34,8 +34,8 @@ public class ReceiptController extends AbstractRestfulController<Receipt> {
 		return receipt.getFileBytes();
 	}
 	
-	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public ResponseEntity<Object> create(@RequestParam("file") MultipartFile file) {
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
 
 		 if (!file.isEmpty()) {
             log.info("File uploaded: {}", file.getOriginalFilename());
@@ -43,8 +43,6 @@ public class ReceiptController extends AbstractRestfulController<Receipt> {
 			try {
 				receipt = new Receipt(file.getContentType(), file.getBytes());
 				receiptService.create(receipt);
-
-
 
 			} catch (IOException e) {
                 log.error("Upload failed because of IO exception");
@@ -57,6 +55,14 @@ public class ReceiptController extends AbstractRestfulController<Receipt> {
             return new ResponseEntity<Object>("Error: upload file is empty", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-		 return new ResponseEntity<Object>("Success", HttpStatus.INTERNAL_SERVER_ERROR);
+		 return new ResponseEntity<Object>("Success", HttpStatus.OK);
 	}
+
+
+    //Changing request mapping so that the file upload method can use this method's default request mapping
+    @Override
+    @RequestMapping(value = "/null", method = RequestMethod.POST)
+    public ResponseEntity<Object> createResource(@RequestBody String json) {
+        return null;
+    }
 }
